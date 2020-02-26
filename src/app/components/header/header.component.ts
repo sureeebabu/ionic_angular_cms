2c0638b15377e8829e8cc93645951cf66d31a1f6
sureeebabu <sureshkumar20141@gmail.com>
2020-02-26 14:10:11 +0530

    sidemenu issue clear in mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
+
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    $('.closeMenu').click(function () {
+      // alert('Wass up!');
+      $($.AdminLTE.options.sidebarToggleSelector).click();
+    });
+  }
 
 }
